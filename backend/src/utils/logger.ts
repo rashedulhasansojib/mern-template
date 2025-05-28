a372283548526cfe79d8bed9a6e39da5abf3f552
Rashedul Hasan Sojib <109899676+rashedulhasansojib@users.noreply.github.com>
2025-05-29 05:55:57 +0600

    test: logger updated with color

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,17 +1,33 @@
+// backend/src/utils/logger.ts
 import { createLogger, format, transports } from 'winston';
+
+const isDev = process.env.NODE_ENV !== 'production';
 
 const logger = createLogger({
   level: 'info',
   format: format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    format.printf(
-      ({ level, message, timestamp }) => `${timestamp} [${level}]: ${message}`
-    )
+    isDev
+      ? format.combine(
+          format.colorize(), // Add colors
+          format.printf(
+            ({ level, message, timestamp }) =>
+              `${timestamp} [${level}]: ${message}`
+          )
+        )
+      : format.printf(
+          ({ level, message, timestamp }) =>
+            `${timestamp} [${level}]: ${message}`
+        )
   ),
   transports: [
     new transports.Console(),
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' }),
+    ...(isDev
+      ? []
+      : [
+          new transports.File({ filename: 'logs/error.log', level: 'error' }),
+          new transports.File({ filename: 'logs/combined.log' }),
+        ]),
   ],
 });
 
