963d3da08f0cced97326a78b734bbafc49d36088
Rashedul Hasan Sojib <109899676+rashedulhasansojib@users.noreply.github.com>
2025-05-28 22:33:35 +0600

    chore(frontend): setup ESLint, Prettier, Husky, and lint-staged for code quality and pre-commit checks

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,28 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+	{ ignores: ['dist', '.eslint.config.js', 'vite.config.ts'] },
+	{
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			sourceType: 'module',
+			globals: globals.browser,
+		},
+		plugins: {
+			react,
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+			'@typescript-eslint': tsPlugin,
+			prettier: prettierPlugin,
+		},
+		rules: {
+			...react.configs.recommended.rules,
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': [
+				'warn',
+				{ allowConstantExport: true },
+			],
+			'prettier/prettier': 'error',
+		},
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+	},
+	prettier // disables conflicting formatting rules
+);
