9e48462a3f9460f1978951ae2b6e4ec153c918cf
Rashedul Hasan Sojib <109899676+rashedulhasansojib@users.noreply.github.com>
2025-05-29 05:48:57 +0600

    chore(root): package.json updated and add winston logger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,24 +1,26 @@
 import cors from 'cors';
 import 'dotenv/config';
-import express, { Request, Response } from 'express';
+import type { Request, Response } from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
+import logger from './utils/logger';
 
 // Connect to MongoDB
 const mongoUri = process.env.MONGODB_CONNECTION_STRING as string;
 if (!mongoUri) {
-	throw new Error(
-		'MONGODB_CONNECTION_STRING is not defined in environment variables'
-	);
+  throw new Error(
+    'MONGODB_CONNECTION_STRING is not defined in environment variables'
+  );
 }
 mongoose
-	.connect(mongoUri)
-	.then(() => {
-		console.log('Connected to MongoDB');
-	})
-	.catch((error) => {
-		console.error('Error connecting to MongoDB:', error);
-		process.exit(1);
-	});
+  .connect(mongoUri)
+  .then(() => {
+    logger.info('Connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  });
 
 // Initialize the Express application
 const app = express();
@@ -27,11 +29,11 @@ app.use(express.json());
 
 // Define a simple test route
 app.get('/test', (req: Request, res: Response) => {
-	res.json({ message: 'Hello, World!' });
+  res.json({ message: 'Hello, World!' });
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT}`);
 });
